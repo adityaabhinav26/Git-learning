e4e9d6fac34d7a07140fa71fcd748f64429df142
adityaabhinav26 <adityaabhinav108@gmail.com>
2023-11-23 21:46:42 +0530

    Change in the print statement

--- check.py
+++ check.py
@@ -3,8 +3,9 @@
 
 for i in range(10):
     print("I will be the Greatest Trader")
-    print("Its running")
+    print("I will do whatever it takes")
     print("You will the richest person on the planet very soon")
+    print("Aditya is the youngest billionare")
 
 
 
